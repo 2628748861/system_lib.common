8d72488ee19b7ce3169ea0f7b5ff28318fdad04b
2628748861 <2628748861@qq.com>
2020-01-09 16:37:22 +0800

    2

---
+++ lib/src/main/java/com/cample/system/lib/common/base/Page.java
@@ -0,0 +1,37 @@
+package com.cample.system.lib.common.base;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Page {
+    private int currentNumber;
+    private int pageSize;
+    private int totalNumber;
+    private List<Object> data;
+
+    public static Page toPage(int currentNumber,int pageSize,List<Object> data){
+        Page page=new Page();
+        if(data!=null){
+
+            page.setCurrentNumber(currentNumber);
+            page.setPageSize(pageSize);
+            page.setTotalNumber(data.size());
+
+            int fromIndex = currentNumber * pageSize;
+            int toIndex = currentNumber * pageSize + pageSize;
+
+            if(fromIndex > data.size()){
+                page.setData(new ArrayList<>());
+            } else if(toIndex >= data.size()) {
+                page.setData(data.subList(fromIndex,data.size()));
+            } else {
+                page.setData(data.subList(fromIndex,toIndex));
+            }
+        }
+        return page;
+    }
+
+}
